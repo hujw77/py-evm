1eaa1cda663c03f1d82a2bc10a9076fe330eb1aa
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2018-03-28 17:38:07 +0200

    Remove block_class from VM
    Fixes #513

--- evm/vm/forks/frontier/__init__.py
+++ evm/vm/forks/frontier/__init__.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 
 from evm.vm import VM
 
-from .blocks import FrontierBlock
 from .vm_state import FrontierVMState
 from .headers import (
     create_frontier_header_from_parent,
@@ -14,7 +13,6 @@ from .headers import (
 FrontierVM = VM.configure(
     __name__='FrontierVM',
     # classes
-    _block_class=FrontierBlock,
     _state_class=FrontierVMState,
     # helpers
     create_header_from_parent=staticmethod(create_frontier_header_from_parent),
